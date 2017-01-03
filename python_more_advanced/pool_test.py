f346d00c3ee5d00b6aa1d276777718ae11a25360
wk633 <wk633@outlook.com>
2017-01-02 22:55:18 -0800

    program using threading

--- python_more_advanced/pool_test.py
+++ python_more_advanced/pool_test.py
@@ -11,7 +11,7 @@ def long_time_task(name):
 if __name__=='__main__':
     print 'Parent process %s.' % os.getpid()
     p = Pool()
-    for i in range(5):
+    for i in range(10):
         p.apply_async(long_time_task, args=(i,))
     print 'Waiting for all subprocesses done...'
     p.close()
